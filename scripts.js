5f9367ca06e3a5443ad2188585b6c2eac541e9bf
CHRZASZCZ_Naulan <nonochrzaszcz@gmail.com>
2021-09-19 21:45:00 +0200

    v1.0

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
-function homeButton() {
-    window.location.href = "acceuil";
-}
+
+/**
+ * It allow to change the current page displayed on the screen.
+ * @param page specify the next page.
+ */
+function redirectionPage(page) { window.location.href = page; }
