30a1c46f72ee0cf4601e27b1eee2e337401be60b
chrzaszcz <nonochrzaszcz@gmail.com>
2021-10-10 00:02:07 +0200

    v1.0

--- scripts.js
+++ scripts.js
@@ -1,41 +1,56 @@
-/* Event button */
-
-let homeButton = document.getElementById("homeButton");
-homeButton.addEventListener("click", homeButtonEvent, false);
 /**
- * On click event
- * FadeOut animation
- * Change HTML page to "acceuil"
- * @param event
- * @return index.html page
+ * CHRZASZCZ Naulan
+ * web-site: https://www.chrz-development.fr/
  */
-function homeButtonEvent(event)
-{
-    let id = setInterval(frame, 3);
-    let opacity = 100;
 
-    function frame() {
-        if (opacity <= 0) { // Finish
-            clearInterval(id);
-            window.location.href = "index.html";
-        } else { // Fadeout animation process
-            opacity -= 3;
-            document.body.style.opacity = opacity + '%';
+
+/**=- Event button -=**/
+
+/**
+ * It allow to create a button with a listener
+ * 2 args:
+ *  - elementId
+ *  - ref (href)
+ */
+class ButtonsEvent
+{
+    constructor(props)
+    {
+        this.elementId = props.elementId;
+        this.button = document.getElementById(this.elementId);
+        this.button.addEventListener("click", run, false);
+
+        function run(event)
+        {
+            let elementInter = setInterval(frame, 3);
+            let opacity = 100;
+
+            function frame()
+            {
+                if (opacity <= 0)
+                {   // Finish
+                    clearInterval(elementInter);
+                    window.location.href = props.ref;
+                } else
+                {   // Fadeout animation process
+                    opacity -= 3;
+                    document.body.style.opacity = opacity + '%';
+                }
+            }
         }
     }
 }
 
-/* ========== */
+// Navigation bar
+new ButtonsEvent({elementId: "homeButton",ref: "index.html"});
+new ButtonsEvent({elementId: "projectButton",ref: "projets"})
+new ButtonsEvent({elementId: "contactButton",ref: "contact"})
+new ButtonsEvent({elementId: "videosButton",ref: "videos"})
 
-
-document.getElementById("projectButton").addEventListener("click", ev => {
-    window.location.href = "projets";
-})
-
-document.getElementById("contactButton").addEventListener("click", ev => {
-    window.location.href = "contact";
-})
 
 document.getElementById("menuIcon").addEventListener("click", ev => {
 
 })
+
+
+
