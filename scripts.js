5788dbf98acdad371e27584f5640fcf151b3b9d8
chrzaszcz <nonochrzaszcz@gmail.com>
2021-10-09 20:23:23 +0200

    v1.0

--- scripts.js
+++ scripts.js
@@ -1,7 +1,32 @@
 /* Event button */
-document.getElementById("homeButton").addEventListener("click", ev => {
-    window.location.href = "acceuil";
-})
+
+let homeButton = document.getElementById("homeButton");
+homeButton.addEventListener("click", homeButtonEvent, false);
+/**
+ * On click event
+ * FadeOut animation
+ * Change HTML page to "acceuil"
+ * @param event
+ * @return index.html page
+ */
+function homeButtonEvent(event)
+{
+    let id = setInterval(frame, 3);
+    let opacity = 100;
+
+    function frame() {
+        if (opacity <= 0) { // Finish
+            clearInterval(id);
+            window.location.href = "index.html";
+        } else { // Fadeout animation process
+            opacity -= 3;
+            document.body.style.opacity = opacity + '%';
+        }
+    }
+}
+
+/* ========== */
+
 
 document.getElementById("projectButton").addEventListener("click", ev => {
     window.location.href = "projets";
