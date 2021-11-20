8d62e0214ec4d722d54ac442bc4a2033692b62a5
nonoc <nonochrzaszcz@gmail.com>
2021-11-20 15:27:13 +0100

    v1.0

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
+
 let nav1button0 = document.getElementById("nav1Button0");
 let nav1button1 = document.getElementById("nav1Button1");
 let nav1button2 = document.getElementById("nav1Button2");
 let nav1button3 = document.getElementById("nav1Button3");
 let nav1button4 = document.getElementById("nav1Button4");
+let navBar1 = document.getElementById("sectionNavBar1");
+
 
 /**
  * Crée une ecoute d'evenement pour un bouton lorsque il est en contact de la souris
@@ -13,6 +16,7 @@ let nav1button4 = document.getElementById("nav1Button4");
  */
 function addEventListenerOverButton(button, id) {
     let navsButtons = [nav1button0, nav1button1, nav1button2, nav1button3, nav1button4];
+
     button.addEventListener("mouseover", ev => {
         for(let n = 0; n < navsButtons.length; n++) {
             if(navsButtons[n].id !== id) {
@@ -25,10 +29,16 @@ function addEventListenerOverButton(button, id) {
     })
 }
 
-addEventListenerOverButton(nav1button0, "nav1Button0")  // Accueil
-addEventListenerOverButton(nav1button1, "nav1Button1")  // Videos
-addEventListenerOverButton(nav1button2, "nav1Button2")  // Projets
-addEventListenerOverButton(nav1button3, "nav1Button3")  // Competance
-addEventListenerOverButton(nav1button4, "nav1Button4")  // Contact
+addEventListenerOverButton(nav1button0, "nav1Button0");     // Accueil
+addEventListenerOverButton(nav1button1, "nav1Button1");     // Videos
+addEventListenerOverButton(nav1button2, "nav1Button2");     // Projets
+addEventListenerOverButton(nav1button3, "nav1Button3");     // Competance
+addEventListenerOverButton(nav1button4, "nav1Button4");     // Contact
 
 
+navBar1.addEventListener("pointerleave", ev => {
+    let navsButtons = [nav1button0, nav1button1, nav1button2, nav1button3, nav1button4];
+
+    for(let n = 0; n < navsButtons.length; n++)
+        navsButtons[n].style.animationName = 'opacityIn';
+})
