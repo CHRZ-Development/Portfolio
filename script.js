42896a3bf18058996aaf5a230cf1d0f9833c2f89
nonoc <nonochrzaszcz@gmail.com>
2021-11-20 15:18:17 +0100

    v1.0

---
+++ script.js
@@ -0,0 +1,34 @@
+
+let nav1button0 = document.getElementById("nav1Button0");
+let nav1button1 = document.getElementById("nav1Button1");
+let nav1button2 = document.getElementById("nav1Button2");
+let nav1button3 = document.getElementById("nav1Button3");
+let nav1button4 = document.getElementById("nav1Button4");
+
+/**
+ * Crée une ecoute d'evenement pour un bouton lorsque il est en contact de la souris
+ * Applique une animation FadeIn ou FadeOut selon où la souris ce situe
+ * @param button document.getElementById ...
+ * @param id Un String qui contient l'ID du bouton concerner (ex: "nav1Button0")
+ */
+function addEventListenerOverButton(button, id) {
+    let navsButtons = [nav1button0, nav1button1, nav1button2, nav1button3, nav1button4];
+    button.addEventListener("mouseover", ev => {
+        for(let n = 0; n < navsButtons.length; n++) {
+            if(navsButtons[n].id !== id) {
+                navsButtons[n].style.animationName = 'opacityOut';
+            } else navsButtons[n].style.animationName = 'opacityIn';
+
+            navsButtons[n].style.animationDuration = '1s';
+            navsButtons[n].style.animationFillMode = 'forwards';
+        }
+    })
+}
+
+addEventListenerOverButton(nav1button0, "nav1Button0")  // Accueil
+addEventListenerOverButton(nav1button1, "nav1Button1")  // Videos
+addEventListenerOverButton(nav1button2, "nav1Button2")  // Projets
+addEventListenerOverButton(nav1button3, "nav1Button3")  // Competance
+addEventListenerOverButton(nav1button4, "nav1Button4")  // Contact
+
+
