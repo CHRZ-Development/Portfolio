53313e297eee0b8104683fa278ba17a8773b864b
nchrzaszcz <naulan.chrzaszcz@ign.fr>
2023-02-27 11:30:18 +0100

    Change script for work with `docker-compose.yml`

--- start-website.sh
+++ start-website.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/sudo bash
 
-
-PORT="80:80"
-DOCKER_PATH="."
-DOCKER_NAME="chrz-website"
-
-echo "Let's start build httpd docker image..."
-docker build --tag $DOCKER_NAME $DOCKER_PATH
-echo "Run $DOCKER_NAME on $PORT..."
-docker run -dit --name $DOCKER_NAME -p $PORT $DOCKER_NAME
+echo "Run website on $WEBSITE_PORT"
+docker compose up
